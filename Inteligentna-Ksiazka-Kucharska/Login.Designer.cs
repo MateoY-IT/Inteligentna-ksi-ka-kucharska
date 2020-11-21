92cddb5b12d51f6aea47797ea0e329bc3802ba8a
Mateusz Korycki <UL0238744@edu.uni.lodz.pl>
2020-11-21 19:37:45 +0100

    Login

--- Inteligentna-Ksiazka-Kucharska/Login.Designer.cs
+++ Inteligentna-Ksiazka-Kucharska/Login.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textlogin = new System.Windows.Forms.TextBox();
+            this.texthaslo = new System.Windows.Forms.TextBox();
+            this.panellogowania = new System.Windows.Forms.Label();
+            this.buttonzaloguj = new System.Windows.Forms.Button();
+            this.niepamietaszhasla = new System.Windows.Forms.LinkLabel();
+            this.ErrorMessage = new System.Windows.Forms.Label();
+            this.zarejestruj = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textlogin
+            // 
+            this.textlogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textlogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textlogin.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textlogin.Location = new System.Drawing.Point(42, 72);
+            this.textlogin.Multiline = true;
+            this.textlogin.Name = "textlogin";
+            this.textlogin.Size = new System.Drawing.Size(464, 30);
+            this.textlogin.TabIndex = 1;
+            this.textlogin.Text = "Login";
+            this.textlogin.Enter += new System.EventHandler(this.textlogin_Enter);
+            this.textlogin.Leave += new System.EventHandler(this.textlogin_Leave);
+            // 
+            // texthaslo
+            // 
+            this.texthaslo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.texthaslo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.texthaslo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.texthaslo.Location = new System.Drawing.Point(42, 131);
+            this.texthaslo.Multiline = true;
+            this.texthaslo.Name = "texthaslo";
+            this.texthaslo.Size = new System.Drawing.Size(464, 30);
+            this.texthaslo.TabIndex = 2;
+            this.texthaslo.Text = "Hasło";
+            this.texthaslo.Enter += new System.EventHandler(this.texthaslo_Enter);
+            this.texthaslo.Leave += new System.EventHandler(this.texthaslo_Leave);
+            // 
+            // panellogowania
+            // 
+            this.panellogowania.AutoSize = true;
+            this.panellogowania.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.panellogowania.Location = new System.Drawing.Point(146, 9);
+            this.panellogowania.Name = "panellogowania";
+            this.panellogowania.Size = new System.Drawing.Size(244, 33);
+            this.panellogowania.TabIndex = 3;
+            this.panellogowania.Text = "Panel Logowania";
+            // 
+            // buttonzaloguj
+            // 
+            this.buttonzaloguj.BackColor = System.Drawing.Color.DarkGray;
+            this.buttonzaloguj.FlatAppearance.BorderSize = 0;
+            this.buttonzaloguj.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.buttonzaloguj.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonzaloguj.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonzaloguj.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonzaloguj.Location = new System.Drawing.Point(42, 194);
+            this.buttonzaloguj.Name = "buttonzaloguj";
+            this.buttonzaloguj.Size = new System.Drawing.Size(464, 40);
+            this.buttonzaloguj.TabIndex = 4;
+            this.buttonzaloguj.Text = "Zaloguj";
+            this.buttonzaloguj.UseVisualStyleBackColor = false;
+            this.buttonzaloguj.Click += new System.EventHandler(this.buttonzaloguj_Click);
+            // 
+            // niepamietaszhasla
+            // 
+            this.niepamietaszhasla.ActiveLinkColor = System.Drawing.Color.Gray;
+            this.niepamietaszhasla.AutoSize = true;
+            this.niepamietaszhasla.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.niepamietaszhasla.LinkColor = System.Drawing.Color.Black;
+            this.niepamietaszhasla.Location = new System.Drawing.Point(39, 250);
+            this.niepamietaszhasla.Name = "niepamietaszhasla";
+            this.niepamietaszhasla.Size = new System.Drawing.Size(147, 17);
+            this.niepamietaszhasla.TabIndex = 5;
+            this.niepamietaszhasla.TabStop = true;
+            this.niepamietaszhasla.Text = "Nie pamiętasz hasła?";
+            // 
+            // ErrorMessage
+            // 
+            this.ErrorMessage.AutoSize = true;
+            this.ErrorMessage.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ErrorMessage.Location = new System.Drawing.Point(39, 164);
+            this.ErrorMessage.Name = "ErrorMessage";
+            this.ErrorMessage.Size = new System.Drawing.Size(99, 16);
+            this.ErrorMessage.TabIndex = 6;
+            this.ErrorMessage.Text = "Error Message";
+            this.ErrorMessage.Visible = false;
+            // 
+            // zarejestruj
+            // 
+            this.zarejestruj.ActiveLinkColor = System.Drawing.Color.Gray;
+            this.zarejestruj.AutoSize = true;
+            this.zarejestruj.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.zarejestruj.LinkColor = System.Drawing.Color.Black;
+            this.zarejestruj.Location = new System.Drawing.Point(359, 250);
+            this.zarejestruj.Name = "zarejestruj";
+            this.zarejestruj.Size = new System.Drawing.Size(91, 17);
+            this.zarejestruj.TabIndex = 8;
+            this.zarejestruj.TabStop = true;
+            this.zarejestruj.Text = "Zarejestruj się";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(580, 330);
+            this.Controls.Add(this.zarejestruj);
+            this.Controls.Add(this.ErrorMessage);
+            this.Controls.Add(this.niepamietaszhasla);
+            this.Controls.Add(this.buttonzaloguj);
+            this.Controls.Add(this.panellogowania);
+            this.Controls.Add(this.texthaslo);
+            this.Controls.Add(this.textlogin);
+            this.Name = "Login";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inteligentna Ksiażka Kucharska";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textlogin;
+        private System.Windows.Forms.TextBox texthaslo;
+        private System.Windows.Forms.Label panellogowania;
+        private System.Windows.Forms.Button buttonzaloguj;
+        private System.Windows.Forms.LinkLabel niepamietaszhasla;
+        private System.Windows.Forms.Label ErrorMessage;
+        private System.Windows.Forms.LinkLabel zarejestruj;
     }
 }
--- Inteligentna-Ksiazka-Kucharska/Login.cs
+++ Inteligentna-Ksiazka-Kucharska/Login.cs
@@ -16,5 +16,62 @@ namespace Inteligentna_Ksiazka_Kucharska
         {
             InitializeComponent();
         }
+
+        private void textlogin_Enter(object sender, EventArgs e)
+        {
+            if(textlogin.Text=="Login"){
+                textlogin.Text = "";
+                textlogin.ForeColor = Color.LightGray;
+
+            }
+        }
+
+        private void textlogin_Leave(object sender, EventArgs e)
+        {
+            if (textlogin.Text == "")
+            {
+                textlogin.Text = "Login";
+                textlogin.ForeColor = Color.DarkGray;
+            }
+        }
+
+        private void texthaslo_Enter(object sender, EventArgs e)
+        {
+            if (texthaslo.Text=="Hasło")
+            {
+                texthaslo.Text = "";
+                texthaslo.ForeColor = Color.LightGray;
+                texthaslo.UseSystemPasswordChar = true;
+
+            }
+        }
+
+        private void texthaslo_Leave(object sender, EventArgs e)
+        {
+            if (texthaslo.Text=="")
+            {
+                texthaslo.Text = "Hasło";
+                texthaslo.ForeColor = Color.DarkGray;
+                texthaslo.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void buttonzaloguj_Click(object sender, EventArgs e)
+        {
+            if (textlogin.Text != "Login")
+            {
+                if (texthaslo.Text != "Hasło") { }
+                else msgError("Proszę podaj hasło.");
+
+            }
+            else msgError("Proszę podać nazwę użytkownika.");
+
+
+        }
+        private void msgError(string msg)
+        {
+            ErrorMessage.Text = "   "+msg;
+            ErrorMessage.Visible = true;
+        }
     }
 }
